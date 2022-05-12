4944eeb0d072234d1d926428fff0ee7bf0c107e6
Karim <karim.gonzalez.ait@gmail.com>
2022-05-12 18:12:55 +0200

    update mapping notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,21 @@ import React from "react"
 import Header from "./Header.jsx"
 import Footer from "./Footer.jsx"
 import Note from "./Note.jsx"
+import Notes from "../Notes.js"
+
 
 
 function App(){
     return(
         <div>
             <Header />
-            <Note />
+            
+            {Notes.map(nota => (<Note 
+                key={nota.id}
+                title={nota.title}
+                content={nota.content}
+            />))}
+
             <Footer />
         </div>
         
